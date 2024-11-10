054fc710dae30a134b6a078cf401cf6f51217486
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2024-11-10 15:15:26 +0800

    Add ProductRepository

---
+++ src/main/java/com/appdev/marketplace/repository/ProductRepo.java
@@ -0,0 +1,24 @@
+package com.appdev.marketplace.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import com.appdev.marketplace.entity.ProductEntity;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+public interface ProductRepo extends JpaRepository<ProductEntity, Integer>{
+	public ProductEntity findByName(String name);
+	public Optional<ProductEntity> findByCode(int code);
+	
+    //public void deleteByCode(int code);
+	
+    @Modifying
+    @Transactional  
+    void deleteByCode(int code);  
+	
+}
