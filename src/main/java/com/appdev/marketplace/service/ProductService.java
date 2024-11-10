42442d6f178d002513545068712791c884a6d17e
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2024-11-10 15:16:54 +0800

    Add ProductService

---
+++ src/main/java/com/appdev/marketplace/service/ProductService.java
@@ -0,0 +1,86 @@
+package com.appdev.marketplace.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.appdev.marketplace.entity.ProductEntity;
+import com.appdev.marketplace.repository.ProductRepo;
+
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import javax.naming.NameNotFoundException;
+ 
+@Service
+@Transactional
+public class ProductService {
+	
+	@Autowired
+	ProductRepo prepo;
+	
+	public ProductService() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//Create of CRUD
+	
+	/*public ProductEntity postProduct(ProductEntity productentity) {
+		return prepo.save(productentity);
+	}*/
+	
+	public void postProduct(String name, String pdtDescription, int qtyInStock, float buyPrice, String imagePath) {
+        // Create and save the product
+        ProductEntity productentity = new ProductEntity();
+        productentity.setName(name);
+        productentity.setPdtDescription(pdtDescription);
+        productentity.setQtyInStock(qtyInStock);
+        productentity.setBuyPrice(buyPrice);
+        productentity.setImagePath(imagePath);  // Store the image path
+
+        prepo.save(productentity);  
+    }
+
+	//Read of CRUD 
+	public List< ProductEntity> getAllProducts(){
+		return prepo.findAll();
+	}
+	
+	public ProductEntity getProductByCode(int code) {
+        Optional<ProductEntity> product = prepo.findById(code);
+        return product.orElse(null); 
+    }
+	
+	//Update of CRUD
+	@SuppressWarnings("finally")
+	public ProductEntity putProductDetails(int code, ProductEntity newProductEntity) {
+		ProductEntity productentity = new ProductEntity();
+		try {
+			productentity = prepo.findByCode(code).get();
+			
+			productentity.setName(newProductEntity.getName());
+			productentity.setPdtDescription(newProductEntity.getPdtDescription());
+			productentity.setQtyInStock(newProductEntity.getQtyInStock());
+			productentity.setBuyPrice(newProductEntity.getBuyPrice());
+			
+		}catch(NoSuchElementException ex) {
+			throw new NameNotFoundException("Product "+code+" not found!");
+		}finally {
+			return prepo.save(productentity);
+		}
+	}
+	
+	//Delete of CRUD
+	public String deleteProduct(int code) {
+		String msg ="";
+		if(prepo.findByCode(code)!=null) {
+			prepo.deleteByCode(code);
+			msg ="Product has been successfully deleted";
+		}else {
+			msg = code+"NOT found!";
+		}
+		return msg;
+	}
+}
