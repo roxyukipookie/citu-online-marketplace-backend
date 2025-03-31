c8a33eb997688c1dd668199828cf8a98786081b5
Chrizmas20 <chrizzaarnie.gales@cit.edu>
2025-03-31 23:52:23 +0800

    change file paths for cross-platforms

--- src/main/java/com/appdev/marketplace/controller/ProductController.java
+++ src/main/java/com/appdev/marketplace/controller/ProductController.java
@@ -51,9 +51,10 @@ public class ProductController {
 	@Autowired
     private AdminService adminService;
 
-	private static final String UPLOAD_DIR = "C:/Users/Lloyd/Downloads/"; // C:/Users/chriz/Downloads/
+	//private static final String UPLOAD_DIR = "C:/Users/Lloyd/Downloads/"; // C:/Users/chriz/Downloads/
 	//private static final String UPLOAD_DIR = "C:/Users/chriz/Downloads/";
-	
+	private static final String UPLOAD_DIR = System.getProperty("user.home") + "/Downloads/";
+
 	@GetMapping("/pendingApproval")
     public List<Map<String, Object>> getPendingApprovalProducts() {
         return adminService.getAllProductsWithSellers();
